e253370c9bb6c528317b28650ed7c7a4a1487e4e
Dillon <dllnloud@aol.com>
2024-09-26 10:16:23 -0400

    scraped all class bis tables

---
+++ IcyBIS/IcyBIS_Items/HunterBIS.lua
@@ -0,0 +1,219 @@
+BEAST_MASTERY_HUNTER_OVERALL_BIS = {
+	itemIDs = {
+		221969, -- P.0.W. x2
+		221143, -- Recurved Hull Impaler
+		212020, -- Lightless Scavenger's Skull
+		225577, -- Sureki Zealot's Insignia
+		221077, -- 'Emergency Stop' Keychain
+		212018, -- Lightless Scavenger's Taxidermy
+		225574, -- Wings of Shattered Sorrow
+		212015, -- Lightless Scavenger's Hide
+		212023, -- Lightless Scavenger's Tunic
+		225581, -- Ky'veza's Covert Clasps
+		219342, -- Glyph-Etched Vambraces
+		219341, -- Glyph-Etched Gauntlets
+		219341, -- Glyph-Etched Gauntlets
+		212414, -- Lost Watcher's Remains
+		221075, -- Reinforced Anti-Intruder Chainmail
+		212019, -- Lightless Scavenger's Stalkings
+		212022, -- Lightless Scavenger's Footpads
+		159379, -- Sure-Foot Sabatons
+		225578, -- Seal of the Poisoned Pact
+		212447, -- Key to the Unseeming
+		225576, -- Writhing Ringworm
+		133282, -- Skardyn's Grace
+		219314, -- Ara-Kara Sacbrood
+		212454, -- Mad Queen's Mandate
+	},
+	messageLeft = "Overall BIS:",
+	messageRight = "Beast Mastery Hunter"
+}
+
+BEAST_MASTERY_HUNTER_RAID_BIS = {
+	itemIDs = {
+		212400, -- Shade-Touched Silencer
+		212020, -- Lightless Scavenger's Skull
+		225577, -- Sureki Zealot's Insignia
+		212429, -- Whispering Voidlight Spaulders
+		225574, -- Wings of Shattered Sorrow
+		212015, -- Lightless Scavenger's Hide
+		212023, -- Lightless Scavenger's Tunic
+		225581, -- Ky'veza's Covert Clasps
+		212415, -- Throne Defender's Bangles
+		212021, -- Lightless Scavenger's Mitts
+		212414, -- Lost Watcher's Remains
+		212019, -- Lightless Scavenger's Stalkings
+		212022, -- Lightless Scavenger's Footpads
+		225578, -- Seal of the Poisoned Pact
+		212447, -- Key to the Unseeming
+		225576, -- Writhing Ringworm
+		221023, -- Treacherous Transmitter
+		212454, -- Mad Queen's Mandate
+	},
+	messageLeft = "Raid BIS:",
+	messageRight = "Beast Mastery Hunter"
+}
+
+BEAST_MASTERY_HUNTER_MYTHIC_PLUS_BIS = {
+	itemIDs = {
+		221143, -- Recurved Hull Impaler
+		212020, -- Lightless Scavenger's Skull
+		221077, -- 'Emergency Stop' Keychain
+		178695, -- Wintersnap Shoulderguards
+		221088, -- Anvilhide Cape
+		212023, -- Lightless Scavenger's Tunic
+		221174, -- Penumbral Rimeguards
+		212021, -- Lightless Scavenger's Mitts
+		221075, -- Reinforced Anti-Intruder Chainmail
+		212019, -- Lightless Scavenger's Stalkings
+		221152, -- Silksteel Striders
+		159379, -- Sure-Foot Sabatons
+		159461, -- Band of the Ancient Dredger
+		221136, -- Devout Zealot's Ring
+		162542, -- Seal of the City Watch
+		133282, -- Skardyn's Grace
+		219314, -- Ara-Kara Sacbrood
+	},
+	messageLeft = "M+ BIS:",
+	messageRight = "Beast Mastery Hunter"
+}
+
+MARKSMANSHIP_HUNTER_OVERALL_BIS = {
+	itemIDs = {
+		212399, -- Splintershot Silkbow
+		178738, -- Rattling Deadeye Hood
+		212448, -- Locket of Broken Memories
+		212018, -- Lightless Scavenger's Taxidermy
+		222817, -- Consecrated Cloak
+		212023, -- Lightless Scavenger's Tunic
+		219342, -- Glyph-Etched Vambraces
+		212021, -- Lightless Scavenger's Mitts
+		225580, -- Accelerated Ascension Coil
+		212019, -- Lightless Scavenger's Stalkings
+		225586, -- Rasha'nan's Grotesque Talons
+		225578, -- Seal of the Poisoned Pact
+		162542, -- Seal of the City Watch
+		212456, -- Void Reaper's Contract
+		221023, -- Treacherous Transmitter
+		212454, -- Mad Queen's Mandate
+		133282, -- Skardyn's Grace
+		220305, -- Ovi'nax's Mercurial Egg
+	},
+	messageLeft = "Overall BIS:",
+	messageRight = "Marksmanship Hunter"
+}
+
+MARKSMANSHIP_HUNTER_RAID_BIS = {
+	itemIDs = {
+		212399, -- Splintershot Silkbow
+		212020, -- Lightless Scavenger's Skull
+		212448, -- Locket of Broken Memories
+		212018, -- Lightless Scavenger's Taxidermy
+		225574, -- Wings of Shattered Sorrow
+		212023, -- Lightless Scavenger's Tunic
+		225581, -- Ky'veza's Covert Clasps
+		212021, -- Lightless Scavenger's Mitts
+		225580, -- Accelerated Ascension Coil
+		212435, -- Liquified Defector's Leggings
+		225586, -- Rasha'nan's Grotesque Talons
+		225578, -- Seal of the Poisoned Pact
+		225576, -- Writhing Ringworm
+		212456, -- Void Reaper's Contract
+		221023, -- Treacherous Transmitter
+		212454, -- Mad Queen's Mandate
+		220305, -- Ovi'nax's Mercurial Egg
+	},
+	messageLeft = "Raid BIS:",
+	messageRight = "Marksmanship Hunter"
+}
+
+MARKSMANSHIP_HUNTER_MYTHIC_PLUS_BIS = {
+	itemIDs = {
+		221074, -- Augmented Refractor Cannon
+		178738, -- Rattling Deadeye Hood
+		178707, -- Trailspinner Pendant
+		212018, -- Lightless Scavenger's Taxidermy
+		133363, -- Troggstitched Drape
+		212023, -- Lightless Scavenger's Tunic
+		221174, -- Penumbral Rimeguards
+		212021, -- Lightless Scavenger's Mitts
+		159386, -- Anchor Chain Girdle
+		212019, -- Lightless Scavenger's Stalkings
+		133293, -- Red Scale Boots
+		221136, -- Devout Zealot's Ring
+		162542, -- Seal of the City Watch
+		133282, -- Skardyn's Grace
+		159623, -- Dead-Eye Spyglass
+		219314, -- Ara-Kara Sacbrood
+	},
+	messageLeft = "M+ BIS:",
+	messageRight = "Marksmanship Hunter"
+}
+
+SURVIVAL_HUNTER_OVERALL_BIS = {
+	itemIDs = {
+		221159, -- Harvester's Interdiction
+		212020, -- Lightless Scavenger's Skull
+		178707, -- Trailspinner Pendant
+		212018, -- Lightless Scavenger's Taxidermy
+		222817, -- Consecrated Cloak
+		212421, -- Goresplattered Membrane
+		219342, -- Glyph-Etched Vambraces
+		212021, -- Lightless Scavenger's Mitts
+		221075, -- Reinforced Anti-Intruder Chainmail
+		212019, -- Lightless Scavenger's Stalkings
+		159379, -- Sure-Foot Sabatons
+		225578, -- Seal of the Poisoned Pact
+		225576, -- Writhing Ringworm
+		133282, -- Skardyn's Grace
+		220305, -- Ovi'nax's Mercurial Egg
+		219312, -- Empowering Crystal of Anub'ikkaj
+	},
+	messageLeft = "Overall BIS:",
+	messageRight = "Survival Hunter"
+}
+
+SURVIVAL_HUNTER_RAID_BIS = {
+	itemIDs = {
+		212389, -- Spire of Transfused Horrors
+		212020, -- Lightless Scavenger's Skull
+		225577, -- Sureki Zealot's Insignia
+		212018, -- Lightless Scavenger's Taxidermy
+		212446, -- Royal Emblem of Nerub-ar
+		212421, -- Goresplattered Membrane
+		212415, -- Throne Defender's Bangles
+		212021, -- Lightless Scavenger's Mitts
+		212414, -- Lost Watcher's Remains
+		212019, -- Lightless Scavenger's Stalkings
+		225586, -- Rasha'nan's Grotesque Talons
+		225578, -- Seal of the Poisoned Pact
+		225576, -- Writhing Ringworm
+		212454, -- Mad Queen's Mandate
+		220305, -- Ovi'nax's Mercurial Egg
+	},
+	messageLeft = "Raid BIS:",
+	messageRight = "Survival Hunter"
+}
+
+SURVIVAL_HUNTER_MYTHIC_PLUS_BIS = {
+	itemIDs = {
+		221159, -- Harvester's Interdiction
+		212020, -- Lightless Scavenger's Skull
+		178707, -- Trailspinner Pendant
+		212018, -- Lightless Scavenger's Taxidermy
+		221088, -- Anvilhide Cape
+		221179, -- Coagulum Cuirass
+		178703, -- Hiveswarm Bracers
+		212021, -- Lightless Scavenger's Mitts
+		221075, -- Reinforced Anti-Intruder Chainmail
+		212019, -- Lightless Scavenger's Stalkings
+		159379, -- Sure-Foot Sabatons
+		162542, -- Seal of the City Watch
+		221141, -- High Nerubian Signet
+		133282, -- Skardyn's Grace
+		219312, -- Empowering Crystal of Anub'ikkaj
+	},
+	messageLeft = "M+ BIS:",
+	messageRight = "Survival Hunter"
+}
+
