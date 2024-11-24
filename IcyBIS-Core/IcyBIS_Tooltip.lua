babe5c67a9e9813399dc615e123fb6ef2e5a99b1
Dillon Louden <djlouden@mix.wvu.edu>
2024-11-23 23:53:22 -0500

    added class color to tooltip class name

--- IcyBIS-Core/IcyBIS_Tooltip.lua
+++ IcyBIS-Core/IcyBIS_Tooltip.lua
@@ -8,7 +8,9 @@ function OnBISTooltip(tooltip, data)
         for _, items_table in ipairs(IcyBIS_TrackedItems) do
             for _, bis_itemID in ipairs(items_table.itemIDs) do
                 if data.lines[1].leftText == C_Item.GetItemInfo(bis_itemID) then
-                    tooltip:AddDoubleLine(items_table.messageLeft, items_table.messageRight)
+                    local classColor = RAID_CLASS_COLORS[items_table.classID]
+                    tooltip:AddDoubleLine(items_table.messageLeft, items_table.messageRight, 1.0, 0.82, 0.0,
+                        classColor.r, classColor.g, classColor.b)
                 end
             end
         end
