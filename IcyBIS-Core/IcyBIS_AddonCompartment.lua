a47f8aebe8074c309e02f8cb25c090ae48017b37
Dillon <dllnloud@aol.com>
2025-02-27 22:35:42 -0500

    added left and right click functionality with party mode toggle

--- IcyBIS-Core/IcyBIS_AddonCompartment.lua
+++ IcyBIS-Core/IcyBIS_AddonCompartment.lua
@@ -1,7 +1,20 @@
 local addonName, ns = ...
 
-function IcyBIS_OnAddonCompartmentClick()
-    ns.ShowSettingsFrame()
+IcyBIS_Settings = IcyBIS_Settings or {}
+
+function IcyBIS_OnAddonCompartmentClick(self, button)
+    if button == "LeftButton" then
+        ns.ShowSettingsFrame()
+    elseif button == "RightButton" then
+        IcyBIS_Settings["party mode checkbox"] = not IcyBIS_Settings["party mode checkbox"]
+        local party_mode_status
+        if IcyBIS_Settings["party mode checkbox"] then
+            party_mode_status = "|cFF00FF00Enabled|r"
+        else
+            party_mode_status = "|cFFFF0000Disabled|r"
+        end
+        ns.fadingToggleMessage("Party Mode: " .. party_mode_status)
+    end
 end
 
 function IcyBIS_OnAddonCompartmentEnter()
@@ -9,8 +22,15 @@ function IcyBIS_OnAddonCompartmentEnter()
     GameTooltip:AddDoubleLine("|cFF38CBFEIcy|r|cFFFF8F00BIS|r", "Version: " .. ns.addonVersion, 1, 0.8, 0, 1, 0.8, 0)
     GameTooltip:AddLine(" ")
     GameTooltip:AddLine("Left-Click to toggle settings")
-    -- GameTooltip:AddLine("Extra")
-    -- GameTooltip:AddLine("Extra2")
+    GameTooltip:AddLine("Right-Click to toggle party mode")
+    local party_mode_status
+    if IcyBIS_Settings["party mode checkbox"] then
+        party_mode_status = "|cFF00FF00Enabled|r"
+    else
+        party_mode_status = "|cFFFF0000Disabled|r"
+    end
+    GameTooltip:AddLine(" ")
+    GameTooltip:AddLine("Party Mode: " .. party_mode_status)
     GameTooltip:Show()
 end
 
